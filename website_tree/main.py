8c160d7252e055666f51c917b80fdbab0b9e207d
Michael Haslam <hreikin@gmail.com>
2022-01-12 19:41:26 +0000

    Insert run_spider function into menu dictionary.

--- website_tree/main.py
+++ website_tree/main.py
@@ -1,11 +1,12 @@
 import simple_menu
 from create_tree import run_create_tree
+from website_tree.spiders.get_urls import run_spider
 
 app_name = "Tree View"
 options = {
     'view': {
         'title':"Run Spider To Generate Links",
-        # 'func': 
+        'func': run_spider
     },
     'search': {
         'title':"Create Tree View",
