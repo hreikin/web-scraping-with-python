8f39dbabf01ffefe79622bd461e810b97b101970
Michael Haslam <hreikin@gmail.com>
2022-01-13 17:36:40 +0000

    Remove base_url variable and switch to using response.url instead.

--- file_download/file_download/spiders/get_files.py
+++ file_download/file_download/spiders/get_files.py
@@ -5,12 +5,11 @@ from file_download.items import DownloadFilesItem
 class GetFilesSpider(scrapy.Spider):
     name = 'get_files'
     allowed_domains = ['docs.python.org']
-    start_urls = ['https://docs.python.org/2/archives/']
-    base_url = 'https://docs.python.org/2/archives/'
+    start_urls = ['https://docs.python.org/2/archives/', 'https://docs.python.org/3/archives/']
 
     def parse(self, response):
         for link in response.xpath('/html/body/pre/a'):
-            all_url = self.base_url + link.xpath('.//@href').get()
+            all_url = response.url + link.xpath('.//@href').get()
             allowed = 'html.zip'
             if allowed in all_url:
                 url = response.urljoin(all_url)
