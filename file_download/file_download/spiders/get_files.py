fd98d9262ac5e267fcfe8e1d1393449485e04af4
Michael Haslam <hreikin@gmail.com>
2022-01-13 18:42:14 +0000

    Create run_spider function so it can be called from the menu.

--- file_download/file_download/spiders/get_files.py
+++ file_download/file_download/spiders/get_files.py
@@ -1,11 +1,11 @@
 import scrapy
-
+from scrapy.crawler import CrawlerProcess
 from file_download.items import DownloadFilesItem
 
 class GetFilesSpider(scrapy.Spider):
     name = 'get_files'
-    allowed_domains = ['docs.python.org']
-    start_urls = ['https://docs.python.org/2/archives/', 'https://docs.python.org/3/archives/']
+    allowed_domains = []
+    start_urls = []
 
     def parse(self, response):
         for link in response.xpath('/html/body/pre/a'):
@@ -16,3 +16,18 @@ class GetFilesSpider(scrapy.Spider):
                 item = DownloadFilesItem()
                 item['file_urls'] = [url]
                 yield item
+
+def run_spider():
+    process = CrawlerProcess(settings={
+        "FEEDS": {
+            "output/output.jl": {"format": "jl"},
+        },
+    })
+    domain = input("Enter the domain, e.g. example.com and press enter: ")
+    # start_urls = []
+    url_to_add = input("Enter the URL, e.g. https://example.com/ and press enter: ")
+    # start_urls.append(url_to_add)
+    process.crawl(GetFilesSpider, start_urls = [url_to_add], allowed_domains = [domain])
+    process.start() # the script will block here until the crawling is finished
+    print("\n\n\n")
+    input("Crawling finished. Press any key to return to the menu: ")
